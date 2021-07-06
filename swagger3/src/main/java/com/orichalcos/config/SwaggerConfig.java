6864999325157021d25ec043d6608dd4e4e83f97
0richalcos <xox.zhe@foxmail.com>
2021-07-07 01:24:50 +0800

    init

--- swagger3/src/main/java/com/orichalcos/config/SwaggerConfig.java
+++ swagger3/src/main/java/com/orichalcos/config/SwaggerConfig.java
@@ -1,9 +1,30 @@
 package com.orichalcos.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
-@Configurable
+@Configuration
 @EnableOpenApi
 public class SwaggerConfig {
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.OAS_30).apiInfo(apiInfo());
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("文档标题")
+                .description("测试的接口文档")
+                .version("v1.0")
+                .termsOfServiceUrl("baidu.com")
+                .contact(new Contact("Orichalcos", "https://github.com/0richalcos", "xox.zhe@foxmail.com"))
+                .build();
+    }
 }
