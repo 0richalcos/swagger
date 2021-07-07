f82370a2399d68ec50896cf1d49bd090bfd6ee4c
0richalcos <xox.zhe@foxmail.com>
2021-07-08 01:01:15 +0800

    2021/07/08/01:01

--- swagger3/src/main/java/com/orichalcos/config/SwaggerConfig.java
+++ swagger3/src/main/java/com/orichalcos/config/SwaggerConfig.java
@@ -2,7 +2,11 @@ package com.orichalcos.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
@@ -14,8 +18,22 @@ import springfox.documentation.spring.web.plugins.Docket;
 public class SwaggerConfig {
 
     @Bean
-    public Docket docket() {
-        return new Docket(DocumentationType.OAS_30).apiInfo(apiInfo());
+    //注：这里注入了 Environment 对象，目的是获取系统环境
+    public Docket docket(Environment environment) {
+
+        //设置要显示 swagger 的环境（dev 或者 test）
+        Profiles profiles = Profiles.of("dev", "test");
+        //判断当前是否处于该环境
+        boolean flag = environment.acceptsProfiles(profiles);
+
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false)
+                .enable(flag)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.orichalcos.controller"))
+                .paths(PathSelectors.any())
+                .build();
     }
 
     public ApiInfo apiInfo() {
@@ -26,5 +44,11 @@ public class SwaggerConfig {
                 .termsOfServiceUrl("baidu.com")
                 .contact(new Contact("Orichalcos", "https://github.com/0richalcos", "xox.zhe@foxmail.com"))
                 .build();
+    }
+
+    @Bean
+    public Docket docket1(){
+        return new Docket(DocumentationType.OAS_30)
+                .groupName("orichalcos");
     }
 }
--- swagger3/src/main/java/com/orichalcos/controller/HelloController.java
+++ swagger3/src/main/java/com/orichalcos/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.orichalcos.contorller;
+package com.orichalcos.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
